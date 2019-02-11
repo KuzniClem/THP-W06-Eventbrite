85e12e91776e00345f3c371d6d8dcc0b50890894
Jin <coutjules@free.fr>
2019-02-11 14:53:17 +0100

    Validations

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,8 @@
 class Attendance < ApplicationRecord
 	belongs_to :user
 	belongs_to :event
+
+	# Validations
+
+	validates :stripe_customer_id,  presence: true
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,20 @@ class Event < ApplicationRecord
 	belongs_to :user
 	has_many :attendances
 	has_many :users, through: :attendances
+
+	# Validations
+
+	validates :start_date, presence: true, if :not_in_past?
+	validates :duration, if :multipe_of_5?, numericality: { greater_than: 0, :only_integer }
+	validates :description, presence: true, length: { minimum: 20 , maximum: 1000 }
+	validates :title, presence: true, length: { minimum: 5, maximum: 140 }
+	validates :price, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 1000, :only_integer }
+	validates :location, presence: true
+
+	def not_in_past?
+		start_date > Time.now
+	end
+	def multipe_of_5?
+		duration % 5 == 0
+	end
 end
